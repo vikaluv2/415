371ddda593f01ce8bb9c5e5293d7d044fb8ae708
Ubuntu <ubuntu@ip-172-31-17-132.ec2.internal>
2019-09-19 11:24:34 +0000

    didn't work

--- learnc/learn.c
+++ learnc/learn.c
@@ -10,6 +10,7 @@ int main(){
     //the white space
     char * space; //pointer for space
     printf("Enter a command =" , stdin);
+    space = strtok (stdin, " ,.-");
     char buffer[1000]; //declare stack array
     gets(buffer);
     printf("result = %s\n",space, buffer);

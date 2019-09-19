c47cf6d948af234adffef28404ca5f0af56dee6a
Ubuntu <ubuntu@ip-172-31-17-132.ec2.internal>
2019-09-19 10:50:33 +0000

    trying to use fgets

--- learnc/learn.c
+++ learnc/learn.c
@@ -3,13 +3,14 @@
 
 int main(){
     
+    
     /*for(int i = 0; i < argc; i++){
         printf("Arg %d: %s\n", i, argv[i]);
     }*/
     
-    printf("Enter a command =");
+    //printf("Enter a command =");
     char buffer[1000]; //declare stack array
-    gets(buffer);
+    fgets(buffer, 1000, stdin);
     printf("result = %s\n", buffer);
     //printf("*buffer = %c\n", *buffer);
     

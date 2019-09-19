5d6952e836801210817c0035ea11480a0ebbfe95
Ubuntu <ubuntu@ip-172-31-17-132.ec2.internal>
2019-09-19 11:21:25 +0000

    adding white space with strtok

--- learnc/learn.c
+++ learnc/learn.c
@@ -7,11 +7,12 @@ int main(){
     /*for(int i = 0; i < argc; i++){
         printf("Arg %d: %s\n", i, argv[i]);
     }*/
-    
+    //the white space
+    char * space; //pointer for space
     printf("Enter a command =" , stdin);
     char buffer[1000]; //declare stack array
     gets(buffer);
-    printf("result = %s\n", buffer);
+    printf("result = %s\n",space, buffer);
     //printf("*buffer = %c\n", *buffer);
     
 }

dd94c20645c89c34e2ef048b1a381d7c22987ff7
Ubuntu <ubuntu@ip-172-31-17-132.ec2.internal>
2019-09-19 12:08:54 +0000

    going off an strtok to know how the white spaces work

--- learnc/learn.c
+++ learnc/learn.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(){
     
     //the white space
+    
+    char str[]= "-this stuff";
     char * space; //pointer for space
     printf("Enter a command =" , stdin);
     space = strtok (stdin, " ,.-");
     char buffer[1000]; //declare stack array
     gets(buffer);
-    printf("result = %s\n", space);
+    printf("result = %s\n", buffer);
     
+    while(space != NULL){
+        printf("%s\n", space);
+        space = strtok (NULL, " ,.-");
+    }
    return 0;
-    
 }

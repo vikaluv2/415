48736ef746f0c474cedfc2f9a7a658898e6b0e5b
Ubuntu <ubuntu@ip-172-31-17-132.ec2.internal>
2019-09-19 10:23:11 +0000

    I am starting from scratch, the array is out of 1000

--- learnc/learn.c
+++ learnc/learn.c
@@ -1,29 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv){
+int main(){
     
-    //printf("Hello World\n");
-    
-    /*int i = 10;
-    printf("I=%d\n", i);
-    int * p; 
-    p= &i; //points p to i
-    *p = *p + 1;
-    printf("i = %d\n", i); */
-    
-    /*int *p = malloc(4);
-    *p = 10;
-    printf("p = %p : *p = %d\n", p, *p);
-    free(p); */ //prints: p = 0x55d72aff1670 : *p = 10
-    
-    for(int i = 0; i < argc; i++){
+    /*for(int i = 0; i < argc; i++){
         printf("Arg %d: %s\n", i, argv[i]);
-    }
+    }*/
     
-    //declare stack array
-    char buffer[1000];
+    char buffer[1000]; //declare stack array
     buffer[0] = 'a';
     printf("Buffer[0] = %c\n", buffer[0]);
+    printf("*buffer = %c\n", *buffer);
     
 }
